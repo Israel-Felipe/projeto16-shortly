de88ed164ff7a55f12ecac96d04867c0452b1995
Israel-Felipe <israelfelipet@gmail.com>
2022-10-15 09:49:03 -0300

    bcrypt insert into signup

--- src/controllers/signup_controllers.js
+++ src/controllers/signup_controllers.js
@@ -1,16 +1,20 @@
 import { connection } from "../database/db.js";
+import bcrypt from "bcrypt";
 
 async function create_user(req, res) {
   const { name, email, password } = req.body;
-  connection
-    .query("INSERT INTO users (name, email, password) VALUES ($1, $2, $3);", [
-      name,
-      email,
-      password,
-    ])
-    .then(() => {
-      res.sendStatus(201);
-    });
+
+  try {
+    const password_hash = bcrypt.hashSync(password, 8);
+    await connection.query(
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3);",
+      [name, email, password_hash]
+    );
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 }
 
 export { create_user };

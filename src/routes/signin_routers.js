3bc955b287a51dfcccc7e049522160a194274693
Israel-Felipe <israelfelipet@gmail.com>
2022-10-15 01:22:33 -0300

    signin and signup create basic

--- src/routes/signin_routers.js
+++ src/routes/signin_routers.js
@@ -2,10 +2,6 @@ import express from "express";
 
 import { login_user } from "../controllers/signin_controllers.js";
 
-/* import {
-  customerBodyValidation,
-} from "../middlewares/customers.middlewares.js"; */
-
 const router = express.Router();
 
 router.post("/signin", login_user);
--- src/routes/signup_routers.js
+++ src/routes/signup_routers.js
@@ -2,10 +2,6 @@ import express from "express";
 
 import { create_user } from "../controllers/signup_controllers.js";
 
-/* import {
-  customerBodyValidation,
-} from "../middlewares/customers.middlewares.js"; */
-
 const router = express.Router();
 
 router.post("/signup", create_user);
